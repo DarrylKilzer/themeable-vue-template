337aa6391d96dce52195fb520c5d1384569e0e73
Darryl Kilzer <darrylkilzer@gmail.com>
2020-03-31 15:36:40 -0600

    add scheme example component

--- src/views/Home.vue
+++ src/views/Home.vue
@@ -1,19 +1,13 @@
 <template>
   <div class="home container-fluid">
     <img alt="Vue logo" src="../assets/logo.png" />
-    <button type="button" name id class="btn btn-primary btn-lg btn-block">primary</button>
-    <button type="button" name id class="btn btn-secondary btn-lg btn-block">secondary</button>
-    <button type="button" name id class="btn btn-success btn-lg btn-block">success</button>
-    <button type="button" name id class="btn btn-danger btn-lg btn-block">danger</button>
-    <button type="button" name id class="btn btn-warning btn-lg btn-block">warning</button>
-    <button type="button" name id class="btn btn-info btn-lg btn-block">info</button>
-    <button type="button" name id class="btn btn-dark btn-lg btn-block">dark</button>
-    <button type="button" name id class="btn btn-light btn-lg btn-block">light</button>
+    <ColorSchemeExample />
   </div>
 </template>
 
 
 <script>
+import ColorSchemeExample from "../components/ColorSchemeExample";
 export default {
   name: "home",
   data() {
@@ -21,7 +15,7 @@ export default {
   },
   computed: {},
   methods: {},
-  components: {}
+  components: { ColorSchemeExample }
 };
 </script>
 
